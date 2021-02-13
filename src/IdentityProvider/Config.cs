5360bf6a0ef870560b7852ecb72898e55aa4d685
Sergey Trofimov <trofimov.developer@gmail.com>
2021-02-13 20:27:32 +0300

    configuring Identity Provider (add Blazor client)

--- src/IdentityProvider/Config.cs
+++ src/IdentityProvider/Config.cs
@@ -26,32 +26,17 @@ namespace IdentityProvider
         public static IEnumerable<Client> Clients =>
             new Client[]
             {
-                // m2m client credentials flow client
                 new Client
                 {
-                    ClientId = "m2m.client",
-                    ClientName = "Client Credentials Client",
-
-                    AllowedGrantTypes = GrantTypes.ClientCredentials,
-                    ClientSecrets = { new Secret("511536EF-F270-4058-80CA-1C89C192F69A".Sha256()) },
-
-                    AllowedScopes = { "scope1" }
-                },
-
-                // interactive client using code flow + pkce
-                new Client
-                {
-                    ClientId = "interactive",
-                    ClientSecrets = { new Secret("49C1A7E1-0C79-4A89-A3D6-A37998FB86B0".Sha256()) },
-
+                    ClientId = "WebUI.Blazor",
                     AllowedGrantTypes = GrantTypes.Code,
-
-                    RedirectUris = { "https://localhost:44300/signin-oidc" },
-                    FrontChannelLogoutUri = "https://localhost:44300/signout-oidc",
-                    PostLogoutRedirectUris = { "https://localhost:44300/signout-callback-oidc" },
-
-                    AllowOfflineAccess = true,
-                    AllowedScopes = { "openid", "profile", "scope2" }
+                    RequirePkce = true,
+                    RequireClientSecret = false,
+                    AllowedCorsOrigins = { "https://localhost:6001" },
+                    AllowedScopes = { "openid", "profile" },
+                    RedirectUris = { "https://localhost:6001/authentication/login-callback" },
+                    PostLogoutRedirectUris = { "https://localhost:6001/" },
+                    Enabled = true
                 },
             };
     }
