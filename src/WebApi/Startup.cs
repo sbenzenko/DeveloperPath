06818cd620d9cec169b6c45ea40495f8649b29be
Simplevolk <simplevolk@mail.ru>
2021-02-19 20:51:23 +0300

    add base swagger

--- src/WebApi/Startup.cs
+++ src/WebApi/Startup.cs
@@ -119,12 +119,15 @@ namespace DeveloperPath.WebApi
             app.UseHealthChecks("/health");
 
             app.UseHttpsRedirection();
+            app.UseOpenApi();
+            app.UseSwaggerUi3();
 
-            app.UseSwaggerUi3(settings =>
-            {
-                settings.Path = "/api";
-                settings.DocumentPath = "/api/specification.json";
-            });
+
+            //app.UseSwaggerUi3(settings =>
+            //{
+            //    settings.Path = "/api";
+            //    settings.DocumentPath = "/api/specification.json";
+            //});
 
             app.UseRouting();
 
