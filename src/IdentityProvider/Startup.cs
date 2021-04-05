a01452aa705f6b4b7dd24f4ffd1e8bf3f21b2d42
Sergey Trofimov <trofimov.developer@gmail.com>
2021-04-05 22:58:01 +0300

    try to fix prod error

--- src/IdentityProvider/Startup.cs
+++ src/IdentityProvider/Startup.cs
@@ -51,7 +51,7 @@ namespace IdentityProvider
                 .AddAspNetIdentity<ApplicationUser>();
 
             // not recommended for production - you need to store your key material somewhere secure
-            builder.AddDeveloperSigningCredential();
+            //builder.AddDeveloperSigningCredential();
 
             services.AddAuthentication();
         }
