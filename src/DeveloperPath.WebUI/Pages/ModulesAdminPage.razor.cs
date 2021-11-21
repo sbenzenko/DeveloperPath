59d762072ad66acfa8007ec771b8050fb57d6e76
Serhii Trofimov <trofimov.developer@gmail.com>
2021-11-21 03:52:20 +0100

    GetModelsQuery - model changed and Icon convertered into SVG

--- src/DeveloperPath.WebUI/Pages/ModulesAdminPage.razor.cs
+++ src/DeveloperPath.WebUI/Pages/ModulesAdminPage.razor.cs
@@ -1,8 +1,15 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using DeveloperPath.Shared.ClientModels;
+using DeveloperPath.Shared.ProblemDetails;
+using DeveloperPath.WebUI.Resources;
 using DeveloperPath.WebUI.Services;
+using DeveloperPath.WebUI.Shared;
+using DeveloperPath.WebUI.UIHelper;
 using Microsoft.AspNetCore.Components;
+using Microsoft.Extensions.Localization;
+using MudBlazor;
 
 namespace DeveloperPath.WebUI.Pages
 {
@@ -11,9 +18,93 @@ namespace DeveloperPath.WebUI.Pages
         [Inject] private ModuleService ModuleService { get; set; }
         public List<Module> Modules { get; set; }
 
+        private State _state;
+
+        [Inject] public IStringLocalizer<LanguageResources> localizer { get; set; }
+
         protected override async Task OnInitializedAsync()
         {
-            Modules = await ModuleService.GetListAsync();
+            _state = State.Loading;
+            try
+            {
+                Modules = await ModuleService.GetListAsync();
+                _state = State.ContentReady;
+            }
+            catch (Exception e)
+            {
+                _state = State.Error;
+            }
+        }
+
+        private async Task ShowModalNewPath()
+        {
+            //var parameters = new DialogParameters { ["IsNew"] = true };
+            //var dialog = DialogService.Show<AddEditPathModal>(localizer["NewPath"], parameters);
+            //var result = await dialog.Result;
+            //if (!result.Cancelled)
+            //{
+            //    await AddNewPathAsync(result.Data as Path);
+            //}
+        }
+
+        private async Task ShowModalEditingPath(Path path)
+        {
+            //var parameters = new DialogParameters { ["IsNew"] = false, ["Path"] = path };
+            //var dialog = DialogService.Show<AddEditPathModal>(localizer["NewPath"], parameters);
+            //var result = await dialog.Result;
+
+            //if (!result.Cancelled)
+            //{
+            //    await EditPathAsync(result.Data as Path);
+            //}
+        }
+
+        private async Task EditPathAsync(Path path)
+        {
+            //try
+            //{
+            //    var result = await PathService.EditPathAsync(path);
+            //    var item = Paths.FirstOrDefault(x => x.Id == result.Id);
+            //    if (item != null)
+            //    {
+            //        item = result;
+            //    }
+            //    SnackbarHelper.PrintSuccess(localizer["PathUpdated"]);
+            //}
+            //catch (ApiError e)
+            //{
+            //    if (e.ProblemDetails.Status == 422)
+            //    {
+            //        SnackbarHelper.PrintErrorDetails((e.ProblemDetails as UnprocessableEntityProblemDetails).Errors);
+            //    }
+            //}
+            //catch (Exception e)
+            //{
+            //    SnackbarHelper.PrintError(e.Message);
+            //}
+            //StateHasChanged();
+        }
+
+        private async Task AddNewPathAsync(Path resultData)
+        {
+            //try
+            //{
+            //    var result = await PathService.AddNewPathAsync(resultData);
+            //    Paths.Add(result);
+            //    SnackbarHelper.PrintSuccess(localizer["PathCreated"]);
+            //}
+            //catch (ApiError e)
+            //{
+            //    if (e.ProblemDetails.Status == 422)
+            //    {
+            //        SnackbarHelper.PrintErrorDetails((e.ProblemDetails as UnprocessableEntityProblemDetails).Errors);
+            //    }
+            //}
+            //catch (Exception e)
+            //{
+            //    SnackbarHelper.PrintError(e.Message);
+            //}
+            //StateHasChanged();
         }
     }
 }
---
+++ src/DeveloperPath.WebUI/UIHelper/State.cs
@@ -0,0 +1,9 @@
+﻿namespace DeveloperPath.WebUI.UIHelper
+{
+    public enum State
+    {
+        Loading,
+        ContentReady,
+        Error
+    }
+}
