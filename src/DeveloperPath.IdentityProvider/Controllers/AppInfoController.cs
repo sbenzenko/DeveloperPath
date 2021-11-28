45636e4f6e213aed0ee8a21ade6fa7d0f80817af
Serhii Trofimov <trofimov.developer@gmail.com>
2021-11-28 18:14:58 +0100

    no message

---
+++ src/DeveloperPath.IdentityProvider/Controllers/AppInfoController.cs
@@ -0,0 +1,17 @@
+﻿using DeveloperPath.BuildInfo;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DeveloperPath.WebApi.Controllers
+{
+    [Route("/application-info")]
+    public class AppInfoController: ControllerBase
+    {
+        [AllowAnonymous]
+        [HttpGet]
+        public ActionResult<BuildInfo.BuildInfo> GetBuildInfo()
+        {
+            return Ok(AppVersionInfo.GetBuildInfo());
+        }
+    }
+}
